f731b7f4cef14f6d56b55a8c4fac2e5b7208294d
sanjanadondapati <99525594+sanjanadmd@users.noreply.github.com>
2023-05-26 11:03:38 +0530

    Refactor sanjana: step 0

--- impls/sanjana/step0_repl.js
+++ impls/sanjana/step0_repl.js
@@ -1,19 +1,22 @@
-const { stdin, stdout } = require('process');
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
 const READ = (str) => str;
 
 const EVAL = (ast) => ast;
 
-const PRINT = (exp) => stdout.write(exp);
+const PRINT = (exp) => exp;
 
-var repl = () => {
-  stdout.write("user> ");
-  READ(EVAL)
-  stdin.setEncoding('utf-8')
-  stdin.on('data', (chunk) => {
-    PRINT(EVAL(READ(chunk)));
-    stdout.write("user> ");
+const rep = (str) => PRINT(EVAL(READ(str)))
+
+const repl = () => {
+  rl.question('user> ', (line) => {
+    console.log(rep(line))
+    repl()
   })
 }
 
-repl();
+repl()
